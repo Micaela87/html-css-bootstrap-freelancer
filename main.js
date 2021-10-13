d5d060c9286a28433a6d3bc48845d3648ae9cb33
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-10-13 14:37:04 +0200

    issue on dropdown menu fixed, all project needs revising

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const hamburgerMenu = document.getElementById('dropdown');
 
-const dropdownMenu = document.getElementById('hidden-menu');
+const dropdownMenu = document.getElementById('menu');
+
+const viewport = window.matchMedia("(max-width: 767px)");
 
 const showMenu = (element) => {
-    element.style.display === 'none' ? element.style.display = 'block' : element.style.display = 'none';
+    element.style.display === 'block' ? element.style.display = 'none' : element.style.display = 'block';
 };
 
 hamburgerMenu.addEventListener('click', function() {
     showMenu(dropdownMenu)
 });
+
